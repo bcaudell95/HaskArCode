7f5022669e10885a43f06b14418a67cab1327080
bcaudell95 <bcaudell95@gmail.com>
2016-12-15 16:35:11 -0600

    more work

--- haskar.hs
+++ haskar.hs
@@ -26,12 +26,23 @@ sortProb (Prob items) = Prob $ sortBy (\ a b -> compare (fst a) (fst b)) items
 
 -- Arithmetic coding operations
 type Interval = (Rational, Rational)
-type UnitMap a = M.Map a Interval
+type IntervalMap a = M.Map a Interval
+type UnitMap a = IntervalMap a
 
-distToUnitMap :: (Ord a) => (Prob a) -> M.Map a Interval
+distToUnitMap :: (Ord a) => (Prob a) -> IntervalMap a
 distToUnitMap (Prob []) = M.empty
 distToUnitMap (Prob ((k,f):xs)) = M.union (M.singleton k (0, f)) (fmap (\(a,b) -> (a+f, b+f)) (distToUnitMap (Prob xs)))
 
-applyIntervalToMap :: (Ord a) => Interval -> UnitMap a -> M.Map a Interval
+applyIntervalToMap :: (Ord a) => Interval -> IntervalMap a -> IntervalMap a
 applyIntervalToMap (start, end) m = fmap (\(a,b) -> (start + (width*a), start + (width*b))) m 
-    where width = end - start
+    where width = (end - start) / ((\ (a,b) -> b - a) . unionOfIntervals . M.elems $ m)
+
+-- Function assumes that the first item in the list is the first interval, the last item is the last interval, and they are adjacent
+unionOfIntervals :: [Interval] -> Interval
+unionOfIntervals intervals = (fst . head $ intervals, snd . last $ intervals)
+
+encodeToInterval :: (Ord a) => [a] -> IntervalMap a -> Interval
+encodeToInterval (x:xs) m = encodeToInterval xs (applyIntervalToMap (unjust mi) m) 
+    where mi = M.lookup x m
+          unjust = (\ (Just a) -> a)
+encodeToInterval [] m = unionOfIntervals . M.elems $ m
